285227e8336f2beed01ffb71166ccbd148b28df7
PhMaguer <philippe.maguer@gmail.com>
2018-03-29 12:17:33 +0200

    server code update

--- server/index.js
+++ server/index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const score_1 = require('../mocks/scoring_1.json')
 const score_2 = require('../mocks/scoring_2.json')
 const app = express()
+const scores = [score_1, score_2]
 
-app.get('/', (request, response) => {
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+app.get('/scores', (request, response) => {
   //setHeader = ('Content-Type', 'text/plain')
-  response.send = ([score_1, score_2])
+  response.send(scores)
 })
 app.listen(3000)
