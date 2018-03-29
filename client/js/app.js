b4ecd51cdbf3a90ddc862674adec18570ae4c5e8
Helene Blanchard <erba0614@gmail.com>
2018-03-29 19:32:55 +0200

    display score with bug

---
+++ client/js/app.js
@@ -0,0 +1,16 @@
+import { showScoreElements } from './components/scores.js'
+
+const main = () => {
+  fetch('http://localhost:3000/scores')
+  .then(res => res.json())
+  .then(scoresReceived)
+}
+
+const scoresReceived = scores => {
+
+  const scoreTable = document.getElementById('scoreList')
+  scoreTable.innerHTML = scores.map(showScoreElements).join('')
+  
+}
+
+main()
---
+++ client/js/components/scores.js
@@ -0,0 +1,10 @@
+export const showScoreElements = score => `
+  <tr>
+
+       <td>${score.userName}</td>
+
+       <td>${score.campus}</td>
+
+       <td>${score.bestScore}</td>
+
+   </tr>`
