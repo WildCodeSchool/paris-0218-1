57352763f8c4c0518efd3039944255ebbfcb112d
Yoann Cribier <ycribier@student.42.fr>
2018-03-31 12:33:20 +0200

    refactor (cleaned code)

--- client/js/app.js
+++ client/js/app.js
@@ -2,15 +2,17 @@ import { showScoreElements } from './components/scores.js'
 
 const main = () => {
   fetch('http://localhost:3000/scores')
-  .then(res => res.json())
-  .then(scoresReceived)
+    .then(res => res.json())
+    .then(scoresReceived)
 }
 
 const scoresReceived = scores => {
-
   const scoreTable = document.getElementById('scoreList')
-  scoreTable.innerHTML = scores.sort((a, b) => b.bestScore - a.bestScore).map(showScoreElements).join('')
   
+  scoreTable.innerHTML = scores
+    .sort((a, b) => b.bestScore - a.bestScore)
+    .map(showScoreElements)
+    .join('')
 }
 
 main()
