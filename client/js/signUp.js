ba8a341616f29254292adf473521efb71079cb98
Louise Foussat <louisefoussat@gmail.com>
2018-04-26 21:01:48 +0200

    signup & signin pages + logic

--- client/js/signUp.js
+++ client/js/signUp.js
@@ -4,20 +4,29 @@ const authForm = document.getElementById('auth_form')
 authForm.addEventListener('submit', event => {
   event.preventDefault()
 
+  // const formData = new FormData(event.target)
+  //
+  // const credentials = {
+  //   username: formData.get('username'),
+  //   email: formData.get('email'),
+  //   password: formData.get('password')
+  // }
+
   const credentials = {
     username: document.getElementById('input_username').value,
     email: document.getElementById('input_email').value,
     password: document.getElementById('input_password').value,
   }
 
-  // console.log(credentials)
-
   const repeatPassword = document.getElementById('input_repeat_password').value
 
   if (credentials.password === repeatPassword) {
-    fetch('http://localhost:3000/authentication', {
+    fetch('http://localhost:3000/sign-up', {
       method: 'post',
-      body: JSON.stringify(credentials)
+      body: JSON.stringify(credentials),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     })
     .then(res => console.log(res))
   } else {
